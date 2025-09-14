edcf576b2e64dd693c3a45df2b99646f07805377
Lawftyg <mymsnadressis@hotmail.com>
2025-09-14 23:56:29 +0200

    pg33

--- getting-starte.c
+++ getting-starte.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
+/* #include <stdbool.h> is needed on older C versions than 23 /*
 /*[[maybe_unused]] is a c23 attribute that will not work on the previous cs*/
 int main(int argc, [[maybe_unused]] char* argv[argc+1]) {
 	double A[5] = {
 		[0] = 9.0,
-	       	[1] = 2.9,
+	       	[1] = 1.9,
 		[4] = 3.E+25,
 		[3] = .00007
 	};
-	for (size_t i = 0; i < 5; ++i){
-		printf("element %zu is %g, \tits square is %g\n", i, A[i], A[i] *A[i]);
+	bool bol = true;
+	size_t i = 0;
+	for (size_t j = i; j < 5; j++){
+
+		printf("element %zu is %g, \tits square is %g\n", j, A[j], A[j] *A[j]);
 
 	}
 	return EXIT_SUCCESS;
