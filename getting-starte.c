c48ec8ccdaba58218f27be4ed131de297edcf5e3
Lawftyg <mymsnadressis@hotmail.com>
2025-09-15 20:40:52 +0200

    p59

--- getting-starte.c
+++ getting-starte.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* #include <stdbool.h> is needed on older C versions than 23 /*
+#include <tgmath.h> /*standard macro fabs */
+/*#include <stdbool.h>  is needed on older C versions than 23 */
 /*[[maybe_unused]] is a c23 attribute that will not work on the previous cs*/
 int main(int argc, [[maybe_unused]] char* argv[argc+1]) {
 	double A[5] = {
@@ -16,5 +17,24 @@ int main(int argc, [[maybe_unused]] char* argv[argc+1]) {
 		printf("element %zu is %g, \tits square is %g\n", j, A[j], A[j] *A[j]);
 
 	}
+
+	int max_iterations = 15;
+	double a = 3.0;
+	double s = 10.0;
+	double x = 0.5;
+	for (size_t i = 0; i < max_iterations; ++i){
+		if(bol && x > 1.0){
+			x = 1.0 /x;
+			continue;
+		}
+		double prod = a*x;
+		if (fabs(1.0 - prod) < s){
+			break;
+		}
+		x *= (2.0 - prod);
+		printf("test %f", x);
+	}
+
+
 	return EXIT_SUCCESS;
 }
---
+++ uti.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	size_t a = 0;
+
+	printf("%zu", (size_t)5%2);
+}
