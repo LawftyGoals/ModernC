6a987000999fb8e94544df2c26c6805d1bd23f90
Lawftyg <mymsnadressis@hotmail.com>
2025-09-16 20:44:42 +0200

    implicit conversion p70

--- sll.c
+++ sll.c
@@ -5,9 +5,15 @@ int main() {
 	
 
 	signed long long sll = -1;
-	signed long long sll2 = 9223372036854775808;
+	signed long long sll2 = 9223372036854775807;
 
 	printf("%lld\n%lld\n", sll, sll2+1);
+
+
+	unsigned int ui = 0xffff'ffff;
+	unsigned int hui = 0x8000'0000;
+	unsigned int mhui = -0x8000'0000;
+	printf("%u ui, %u hui, %u mhui\n", ui, hui, mhui);
 
 	return EXIT_SUCCESS; 
 
