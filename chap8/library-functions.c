8d3ff8a8c5ddf63147988b1ea221406b3054b281
Lawftyg <mymsnadressis@hotmail.com>
2025-09-24 22:33:25 +0200

    p182

--- chap8/library-functions.c
+++ chap8/library-functions.c
@@ -1,5 +1,13 @@
+#if -1 & 3 != 3
+# error "This compiler does not use Two's complement"
+#endif
+#define TWOS_COMPLEMENT 1
+
+#include <limits.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdckdint.h>
 
 double my_strtod(char *s){
   double beforePeriod = 0.0;
@@ -33,14 +41,59 @@ double my_strtod(char *s){
       ++idx;
     }
   }
-  printf("%d\n", sizeof(s));
+  printf("%lu\n", sizeof(s));
   printf("%f\n", beforePeriod + afterPeriod);
   return beforePeriod + afterPeriod;
 }
-int main(int argc, char* argv[argc+1]) {
 
+void test_int_arith(){
+/*stdlib*/
+  auto res = div(13, 5);
+  printf("abs %d, div %d rem %d\n", abs(-10), res.quot, res.rem); 
+
+  /*stdckdint*/
+  unsigned overflow = 0;              //limits.h
+  bool overflowed = ckd_add(&overflow, UINT_MAX, UINT_MAX);
+  printf("Overflow flag %s, result %x\n", (overflowed ? "true": "false"), overflow); 
+}
+
+int manual_puts(char const s[static 1]) {
+  for(size_t i = 0; s[i]; ++i) {
+    if(putchar(s[i]) == EOF) return EOF;
+  }
+  if(putchar('\n') == EOF) return EOF;
+  return 0;
+}
+
+int stdout_puts_manually(char const s[static 1]) {
+  if(fputs(s,    stdout) == EOF) return EOF;
+  if(fputc('\n', stdout) == EOF) return EOF;
+  return 0;
+}
+
+int main(int argc, char* argv[argc+1]) {
+  printf("twos %d\n", TWOS_COMPLEMENT);
   my_strtod("-10523.0203");
   printf("%s, %d\n", argv[0], argc);
-  return 0;
+  
+  test_int_arith();
+  manual_puts("testing this");
+
+  stdout_puts_manually("working on this");
+  
+  FILE* logfile = fopen("bob.txt", "a");
+  if(!logfile){
+    perror("fopen failed");
+    return EXIT_FAILURE;
+  }
+  fputs("feeling fine today\n", logfile);
+
+  if(!freopen("mylog.txt", "a", stdout)){
+    perror("freopen failed");
+    return EXIT_FAILURE;
+  }
+  puts("feelings are fine today");
+
+  return EXIT_SUCCESS;
 }
 
