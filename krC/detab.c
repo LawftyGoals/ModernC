c82ba6cf29a4a06b1ddfc99073139428548f860d
Lawftyg <mymsnadressis@hotmail.com>
2025-10-05 21:26:59 +0200

    ex1-20 p37

---
+++ krC/detab.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int tabreplace(char line[MAXLINE]);
+
+int main() {
+
+  char line[MAXLINE];
+
+  printf("%d", 0 % 8);
+  while(tabreplace(line) > 0) {
+    printf("%s", line);
+  }
+
+  printf("\ttab1\n");
+  printf("ta\tb2\n");
+  printf("1234567890\n");
+  return 0;
+}
+
+
+int tabreplace(char s[MAXLINE]){
+  int i = 0;
+  char c;
+  
+  while (i < MAXLINE && (c = getchar()) != EOF && c != '\n'){
+    
+    if(c == '\t') {
+      int j;
+      int mod = i % 8; 
+      for(j = 0; j < (mod ? 8 - mod : 8); ++j){
+        s[i] = ' ';
+        ++i;
+      }
+    } else {
+      s[i] = c;
+      ++i;
+    }
+  }
+  if(c == '\n') {
+     s[i] = '\n';
+     ++i;
+     s[i] = '\0';
+  }
+
+  return i;
+}
+
