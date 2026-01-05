38847c8ae9c59436180e051dd421d34f51650440
oliver.sokol <oliver.sokol@nhi.no>
2026-01-05 09:51:30 +0100

    I dunno what I did here

--- krC/keyword-count.c
+++ krC/keyword-count.c
@@ -10,7 +10,22 @@ struct key {
   char *word;
   int count;
 } keytab[] = {
-  {"auto", 0}, {"break", 0}, {"case", 0}, {"char", 0}, {"const", 0}, {"continue", 0}, {"default", 0}, {"do", 0}, {"double", 0}, {"else", 0}, {"enum", 0}, {"extern", 0}, {"float", 0}, {"for", 0}, {"goto", 0}, {"if", 0}, {"inline", 0}, {"int", 0}, {"long", 0}, {"register", 0},  {"restrict", 0}, {"return", 0}, {"short", 0}, {"signed", 0}, {"sizeof", 0}, {"static", 0}, {"struct", 0}, {"switch", 0}, {"typedef", 0}, {"union", 0}, {"unsigned", 0}, {"void", 0}, {"volatile", 0}, {"while", 0}, {"_Alignas", 0}, {"_Alignof", 0}, {"_Atomic", 0}, {"_Bool", 0}, {"_Complex", 0}, {"_Generic", 0}, {"_Imaginary", 0}, {"_Noreturn", 0}, {"_Static_assert", 0}, {"_Thread_local", 0}, {"#define", 0}, {"#elif", 0}, {"#else", 0}, {"#endif", 0}, {"#error", 0}, {"#if", 0}, {"#ifdef", 0}, {"#ifndef", 0}, {"#include", 0}, {"#line", 0}, {"#pragma", 0}, {"#undef", 0 }};
+  {"#define", 0}, {"#elif", 0}, {"#else", 0}, 
+  {"#endif", 0}, {"#error", 0}, {"#if", 0}, {"#ifdef", 0}, 
+  {"#ifndef", 0}, {"#include", 0}, {"#line", 0}, {"#pragma", 0}, 
+  {"#undef", 0 }, {"_Alignas", 0}, {"_Alignof", 0}, 
+  {"_Atomic", 0}, {"_Bool", 0}, {"_Complex", 0}, {"_Generic", 0}, 
+  {"_Imaginary", 0}, {"_Noreturn", 0}, {"_Static_assert", 0}, 
+  {"_Thread_local", 0}, {"auto", 0}, {"break", 0}, {"case", 0}, {"char", 0}, 
+  {"const", 0}, {"continue", 0}, {"default", 0}, {"do", 0}, 
+  {"double", 0}, {"else", 0}, {"enum", 0}, {"extern", 0}, 
+  {"float", 0}, {"for", 0}, {"goto", 0}, {"if", 0}, 
+  {"inline", 0}, {"int", 0}, {"long", 0}, {"register", 0},  
+  {"restrict", 0}, {"return", 0}, {"short", 0}, {"signed", 0}, 
+  {"sizeof", 0}, {"static", 0}, {"struct", 0}, {"switch", 0}, 
+  {"typedef", 0}, {"union", 0}, {"unsigned", 0}, {"void", 0}, 
+  {"volatile", 0}, {"while", 0}
+};
 
 
 int binsearch(char*, struct key *, int);
@@ -22,10 +37,9 @@ void ungetch(char);
 int main(void){
   int n;
   char word[MAXWORD];
-  printf("%ld", NKEYS);
 
   while(getword(word, MAXWORD) != EOF){
-    if(isalpha(word[0]))
+    if(isalpha(word[0]) || word[0] == '#' || word[0] == '_')
       if((n = binsearch(word, keytab, NKEYS)) >= 0)
         keytab[n].count++;
   }
@@ -46,11 +60,9 @@ int binsearch(char *word, struct key tab[], int n){
     mid = (low + high) / 2;
     if((cond = strcmp(word, tab[mid].word)) < 0){
       high = mid - 1;
-      printf("%s - %s ", word, tab[mid].word);
     }
     else if(cond > 0){
       low = mid + 1;
-      printf("%s - %s ", word, tab[mid].word);
     }
     else return mid;
   }
@@ -64,7 +76,7 @@ int getword(char *word, int lim){
   while(isspace(c = getch()));
 
   if(c == '/' && (c = getch()) == '/'){
-    while((c = getch()) != '\0');
+    while((c = getch()) != '\0' && c != '\n');
   }
 
   if(c != EOF) *w++ = c;
@@ -110,4 +122,7 @@ void ungetch(char c){
   if(charbufp >= MAXLEN) printf("NOT ENOUGH SPACE IN CHARBUFF");
   else charbuf[charbufp++] = c;
 }
+
+
+
 
