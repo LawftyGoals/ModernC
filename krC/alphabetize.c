eb83c21f098126c7ab7f2d44f92c30c505effc77
oliver.sokol <oliver.sokol@nhi.no>
2026-01-06 12:22:50 +0100

    selects variable and adds it now

--- krC/alphabetize.c
+++ krC/alphabetize.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAXCHARBUFF 1000
 #define MAXINPLIM 1000
@@ -12,78 +13,120 @@ char getch(void);
 void ungetch(char);
 int getword(char *s, int lim);
 char *mallocword(char *);
-int binsearch(char**, size_t, char*);
-char *binsearchp(char**, size_t, char*);
+int binsearch(char **, size_t, char *);
+char *binsearchp(char **, size_t, char *);
 
-struct variablekey { char *word; size_t count; struct variablekey *left; struct variablekey *right;};
+struct variablekey *keymalloc(void);
+struct variablekey *addnode(char *, struct variablekey *);
 
+void printtree(struct variablekey *);
+
+struct variablekey
+{
+  char *word;
+  size_t count;
+  struct variablekey *left;
+  struct variablekey *right;
+};
 
 size_t LIST_LEN = 7;
-char *DATA_TYPES[] = { "bool", "char", "double", "float", "int", "long", "short" };
+char *DATA_TYPES[] = {"bool", "char", "double", "float", "int", "long", "short"};
 
-int main(void) {
-  //char *words[MAXINPLIM];
-  char *variables[MAXINPLIM];
-  //char word[MAXINPLIM];
-  struct variablkey rootkey;
+int main(void)
+{
+  // char *words[MAXINPLIM];
+  // char *variables[MAXINPLIM];
+  char word[MAXINPLIM];
+  struct variablekey *rootkey = NULL;
   char fl;
   int i = 0;
 
-
-  struct variablekey rootnode;
-
   char *wordli;
+  bool isvariable = false;
 
-  while((fl = getword(word, MAXINPLIM)) != EOF){
-    //printf("%d\n", binsearch(DATA_TYPES, LIST_LEN-1, word));
-    if((wordli = binsearchp(DATA_TYPES, LIST_LEN-1, word)) != NULL){
-      printf("%s\n", wordli);
-      while((fl = getword(word, MAXINPLIM)) != EOF && (wordli = binsearchp(DATA_TYPES, LIST_LEN-1, word)) != NULL);
-      
+  while ((fl = getword(word, MAXINPLIM)) != EOF)
+  {
+    // printf("%d\n", binsearch(DATA_TYPES, LIST_LEN-1, word));
+    if ((wordli = binsearchp(DATA_TYPES, LIST_LEN - 1, word)) != NULL)
+    {
+      isvariable = true;
+    }
+    if (isvariable && wordli == NULL)
+    {
+      rootkey = addnode(word, rootkey);
+      isvariable = false;
     }
     i++;
   }
+  printf("outofwhile\n");
+  printtree(rootkey);
 
   return 0;
 }
 
-struct variablekey *addNode(char *s, struct variablekey *p){
+void printtree(struct variablekey *rootkey)
+{
+  if (rootkey != NULL)
+  {
+    printtree(rootkey->left);
+    printf("%s\n", rootkey->word);
+    printtree(rootkey->right);
+  }
+}
+
+struct variablekey *addnode(char *s, struct variablekey *p)
+{
   int cond;
-  if(p == NULL){
-    p = keyalloc();
+  if (p == NULL)
+  {
+    p = keymalloc();
     p->word = mallocword(s);
     p->count = 1;
-    p->left = p-right = NULL;
-  } else if ((cond = strcmp(s, p->word)) == 0) {
-    p-count++;
-  } else if (cond < 0) {
-    p->left = addtree(p->left, s);
-  } else {
-    p->right = addtree(p->right, s);
+    p->left = p->right = NULL;
+  }
+  else if ((cond = strcmp(s, p->word)) == 0)
+  {
+    p->count++;
+  }
+  else if (cond < 0)
+  {
+    p->left = addnode(s, p->left);
+  }
+  else
+  {
+    p->right = addnode(s, p->right);
   }
   return p;
 }
 
-char* mallocword(char *word){
-  char *pword = (char *) malloc(strlen(word) + 1);
+struct variablekey *keymalloc()
+{
+  return (struct variablekey *)malloc(sizeof(struct variablekey));
+}
 
-  if(pword != NULL)
+char *mallocword(char *word)
+{
+  char *pword = (char *)malloc(strlen(word) + 1);
+
+  if (pword != NULL)
     strcpy(pword, word);
-  
+
   return pword;
 }
 
-char *binsearchp(char* list[], size_t listlen, char *word){
+char *binsearchp(char *list[], size_t listlen, char *word)
+{
   char **start = list;
   char **end = list + listlen;
   char **mid;
   int cond;
 
-  while(start <= end){
+  while (start < end)
+  {
     mid = start + (end - start) / 2;
-    if((cond = strcmp(word, *mid)) < 0)
+    if ((cond = strcmp(word, *mid)) < 0)
       end = mid;
-    else if(cond > 0)
+    else if (cond > 0)
       start = mid + 1;
     else
       return *mid;
@@ -92,64 +135,71 @@ char *binsearchp(char* list[], size_t listlen, char *word){
   return NULL;
 }
 
-
-//listlen = length of list - 1
-int binsearch(char *list[], size_t listlen, char *word){
+// listlen = length of list - 1
+int binsearch(char *list[], size_t listlen, char *word)
+{
   size_t start = 0;
   size_t end = listlen;
   size_t mid;
   int cond;
 
-  while(start <= end){
+  while (start <= end)
+  {
     mid = (start + end) / 2;
-    if((cond = strcmp(word, list[mid])) < 0){
+    if ((cond = strcmp(word, list[mid])) < 0)
+    {
       end = mid - 1;
-    } else if(cond > 0) {
+    }
+    else if (cond > 0)
+    {
       start = mid + 1;
-    } else
+    }
+    else
       return mid;
   }
 
   return -1;
 }
- 
-
-
-
 
 char charbuf[MAXCHARBUFF];
 size_t charbufp = 0;
 
-char getch(void){
+char getch(void)
+{
   return charbufp > 0 ? charbuf[--charbufp] : getchar();
 }
 
-void ungetch(char c){
-  if(charbufp >= MAXCHARBUFF) printf("getch char buffer limit reached");
-  else charbuf[charbufp++] = c;
+void ungetch(char c)
+{
+  if (charbufp >= MAXCHARBUFF)
+    printf("getch char buffer limit reached");
+  else
+    charbuf[charbufp++] = c;
 }
 
-
-int getword(char *s, int lim){
+int getword(char *s, int lim)
+{
   char c;
-  
+
   char *sp = s;
 
-  while(isspace(c = getch()))
+  while (isspace(c = getch()))
     ;
 
-  if(c != EOF)
+  if (c != EOF)
     *sp++ = c;
-  if(!isalpha(c) && c != '_' && c != '#') {
+  if (!isalpha(c) && c != '_' && c != '#')
+  {
     *sp = '\0';
     return c;
   }
 
-
-  for( ; --lim > 0; sp++) {
-    if((*sp = getch()) == '_' && isalpha(c = getch()))
+  for (; --lim > 0; sp++)
+  {
+    if ((*sp = getch()) == '_' && isalpha(c = getch()))
       ungetch(c);
-    else if(!isalnum(*sp)) {
+    else if (!isalnum(*sp))
+    {
       ungetch(*sp);
       break;
     }
@@ -158,4 +208,3 @@ int getword(char *s, int lim){
 
   return s[0];
 }
-
