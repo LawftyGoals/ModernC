077c91dfaeb76e96baa11f4031b960ee986ef426
oliver.sokol <oliver.sokol@nhi.no>
2026-01-19 15:50:31 +0100

    e.6-5 fin

--- krC/hashtable.c
+++ krC/hashtable.c
@@ -1,17 +1,58 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define HASHSIZE 101
 
+
 struct nlist {
-  struct nlinst *next;
+  struct nlist *next;
   char *name;
   char *defn;
-}
+};
 
-static struct nlinst *hastab[HASHSIZE];
+static struct nlist *hashtab[HASHSIZE];
 
 
 unsigned hash(char *s);
 struct nlist *lookup(char *s);
+struct nlist *install(char* name, char *defn);
 char *stralloc(char *);
+
+void undef(char *name);
+void zeroList();
+void printlist();
+
+int main(void){
+  zeroList();
+  install("shoulder", "testing");
+  printlist();
+  undef("shoulder");
+  printlist();
+
+  return 0;
+}
+
+void printlist(){
+  struct nlist *target;
+  for(uint16_t i = 0; i < HASHSIZE; i++){
+    if((target = hashtab[i]) != NULL){
+      printf("%s\n", target->name);
+      while(target->next != NULL){
+        printf("%s\n", target->next->name);
+        target = target->next;
+      }
+    }
+  }
+}
+
+void zeroList(){
+  for(uint16_t i = 0; i < HASHSIZE; i++){
+    hashtab[i] = NULL;
+  }
+}
 
 unsigned hash(char *s){
   unsigned hashval;
@@ -24,7 +65,7 @@ unsigned hash(char *s){
 struct nlist *lookup(char *s){
   struct nlist *np;
 
-  for(np = hashtab[hash(s)]; np != NULL; np = np-next){
+  for((np = hashtab[hash(s)]); np != NULL; (np = np->next)){
     if(strcmp(s, np->name) == 0)
       return np;
   }
@@ -58,7 +99,51 @@ struct nlist *install(char *name, char *defn){
   return np;
 }
 
+void undef(char *name){
+  struct nlist *np;
+  struct nlist *prevnp = NULL;
 
+  unsigned hashval = hash(name);
+
+  for((np = hashtab[hashval]); np != NULL; np = np->next){
+    if(strcmp(np->name, name) == 0){
+      free(np->name);
+      free(np->defn);
+
+      if(prevnp == NULL){
+        hashtab[hashval] = np->next;
+      } else {
+        prevnp->next = np->next;
+      }
+
+      free(np);
+
+      return;
+    }
+    prevnp = np;
+  }
+
+  /*
+
+  if(np != NULL){
+    free(np->name);
+    free(np->defn);
+    
+    if(np->next != NULL){
+      nextnp = np->next;
+
+      np->name = nextnp->name;
+      np->defn = nextnp->defn;
+      np->next = nextnp->next;
+
+      free(nextnp);
+    } else {
+      free(np);
+
+    }
+  }*/
+}
+    
 
 
 
