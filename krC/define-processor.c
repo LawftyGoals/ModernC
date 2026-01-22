4af171019f2b488582116136eeed31c03386168a
Lawftyg <mymsnadressis@hotmail.com>
2026-01-22 13:06:28 +0100

    e.6-6 finished

--- krC/define-processor.c
+++ krC/define-processor.c
@@ -1,4 +1,6 @@
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -18,31 +20,77 @@ unsigned hash(char *name);
 struct nlist *lookup(char *name);
 struct nlist *addtotable(char *name, char *defn);
 
-
+char *stralloc(char *s);
 
 char getch(void);
 void ungetch(char c);
 
 char getword(char *word, uint16_t lim);
+void printlist(void);
 
 int main(void)
 {
   char word[MAXWORDLIM];
   char lc = ' ';
 
+  char var[MAXWORDLIM];
+  char defn[MAXWORDLIM];
+  struct nlist *found;
+
   while ((lc = getword(word, MAXWORDLIM)) != EOF)
   {
+    if(lc == '#' && strcmp(word, "#define") == 0){
+      getword(var, MAXWORDLIM);
+      getword(defn, MAXWORDLIM);
+      addtotable(var, defn);
+    } else if((found = lookup(word)) != NULL)
+      strcpy(word, found->defn);
     printf("%s\n", word);
   }
 
+  printlist();
+
   return 0;
 }
+void printlist(){
+   struct nlist *target;
+   for(uint16_t i = 0; i < HASHLIM; i++){
+     if((target = hashtable[i]) != NULL){
+        printf("%s - %s\n", target->name, target->defn);
+     while(target->next != NULL){
+        printf("%s - %s\n", target->next->name, target->next->defn);
+        target = target->next;
+      }
+    }
+  }
+}
+
+struct nlist *addtotable(char *name, char *defn){
+  unsigned hashval = hash(name);
+  struct nlist *np = lookup(name);
+  if(np == NULL){
+    np = (struct nlist *)malloc(sizeof(*np));
+    if(np == NULL || (np->name = stralloc(name)) == NULL)
+      return NULL;
+    np->next = hashtable[hashval];
+    hashtable[hashval] = np;
+  } else {
+    free(np->defn);
+  }
+  if((np->defn = stralloc(defn)) == NULL) {
+      return NULL;
+  }
+
+  return np;
+}
+
+
 
 
 unsigned hash(char *name){
   unsigned total = 0;
   for( ; *name != '\0'; name++){
-    total = name + 31 * total;
+    total = *name + 31 * total;
   }
   return total % HASHLIM;
 }
@@ -52,14 +100,20 @@ struct nlist *lookup(char *name){
 
   struct nlist *np;
 
-  for(np = hashtab[hashval]; np != NULL; np = np->next){
+  for(np = hashtable[hashval]; np != NULL; np = np->next){
     if(strcmp(np->name, name) == 0)
       return np;
   }
 
-  retrurn NULL;
+  return NULL;
 }
 
+char *stralloc(char *s){
+  char *str = (char *)malloc(strlen(s) + 1);
+  if(str != NULL)
+    strcpy(str, s);
+  return str;
+}
 
 
 char getword(char *word, uint16_t lim)
